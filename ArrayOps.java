07c8c9ee551d2657c67d4c6d83c0c070b6bc6c80
pokiebob <ccursetjee@gmail.com>
2020-10-25 23:19:49 -0400

    sum function

---
+++ ArrayOps.java
@@ -0,0 +1,13 @@
+public class ArrayOps {
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
